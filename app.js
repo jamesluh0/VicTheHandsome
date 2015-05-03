13af9cb33b4f21e9322b085926c916e3ec82c50e
jamesluh0 <jamesluh0@hotmail.com>
2015-05-03 18:40:16 +0800

    for v

---
+++ app.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+	var ifClick = 0;
+	var target_last, target_now;
+
+	$('li').click(function(event){
+		
+		if(ifClick==0)
+		{			
+			$('.wrap').fadeIn(500);
+			$('.content').fadeIn(1000);
+			ifClick = 1;
+			target_last = event.target;
+			switch(event.target.id)
+			{
+
+				case "gallery": 
+				{
+					$('.content').load('gallery.html');
+					break;
+				}
+				case "about": 
+				{
+					console.log('a');
+					break;
+				}
+				case "love": 
+				{
+					console.log('l');
+					break;
+				}
+			}
+
+		}
+		else
+		{
+			
+			console.log(target_last);
+			if(this == target_last)
+				{
+					$('.wrap').fadeOut(500);
+					$('.content').fadeOut(1000);
+				}
+			else
+					{
+						switch(event.target.id)
+						{
+
+							case "gallery": 
+							{
+								$('.content').load('gallery.html');
+								break;
+							}
+							case "about": 
+							{
+								console.log('a');
+								break;
+							}
+							case "love": 
+							{
+								console.log('l');
+								break;
+							}
+						}
+				}
+			ifClick = 0;
+		}
+	})
+
+	$('body').on('click' , '.close', function(){
+			$('.wrap').fadeOut(500);
+			$('.content').fadeOut(1000);
+			ifClick = 0;
+	})
+});
